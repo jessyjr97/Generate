7cac77b3d54da7ad74a5624a986ebf5d2e11ef2d
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2018-11-19 17:03:05 -0500

    Noise dédié pour le gisement

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -20,12 +20,20 @@ public class MapGenerator : MonoBehaviour {
 
     public Tilemap tilemap;
     public TerrainType[] regions;
+    public TerrainType[] gisements;
 
     private TileBase FindTileFromRegion(float valeur) {
         for (int i = 0; i < regions.Length; i++)
             if (valeur <= regions[i].value)
                 return regions[i].tile;
         return regions[0].tile;
+    }
+
+    private TileBase FindTileFromGisement(float valeur) {
+        for (int i = 0; i < gisements.Length; i++)
+            if (valeur <= gisements[i].value)
+                return gisements[i].tile;
+        return gisements[0].tile;
     }
 
     public void SetTileMap(TileBase[] customTilemap) {
@@ -46,11 +54,17 @@ public class MapGenerator : MonoBehaviour {
 
     public void GenerateMapPerlinNoise() {
         var noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+        var noiseMapGisement = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed + 1, noiseScale * 10, octaves, persistance, lacunarity, offset);
         TileBase[] customTilemap = new TileBase[mapWidth * mapHeight];
+        TileBase[] customTilemapGisement = new TileBase[mapWidth * mapHeight];
         for (int y = 0; y < mapWidth; y++)
             for (int x = 0; x < mapHeight; x++) {
                 var value = noiseMap[x, y];
-                customTilemap[y * mapWidth + x] = FindTileFromRegion(value);
+                var valueGisement = noiseMapGisement[x, y];
+                if (value >= 0.8f)
+                    customTilemap[y * mapWidth + x] = FindTileFromGisement(valueGisement);
+                else
+                    customTilemap[y * mapWidth + x] = FindTileFromRegion(value);
             };
         SetTileMap(customTilemap);
     }
